61cd4478fe5da7f5bb0158b4d6431845144d70a0
raja09569 <rajashekar.pandu@gmail.com>
2018-02-04 17:26:07 +0530

    Admin dashbaord showing customer trips is over

--- web/Admin/Dashboard/cabRides.php
+++ web/Admin/Dashboard/cabRides.php
@@ -7,8 +7,8 @@
 	<!-- Tab panes from left menu -->
 	<div class="tab-content5">		
 		<!-- CAB RIDES TAB -->					  
-		<div class="tab-pane" id="cabrides">
-			<div style="height:100vh;overflow:auto;">
+		<div id="cabrides">
+			<div>
 				<table class="flatTable">
 					<tr class="titleTr">
 						<td colspan="2" class="titleTd">Cab Rides</td>
@@ -28,6 +28,11 @@
 					</tr>
 					<tbody id="listcabRides"></tbody>
 				</table>
+				<div class="pagination">
+					<ul>
+						
+					</ul>
+				</div>
 			</div>
 		</div>
 		<!-- END OF CAB RIDES TAB -->
@@ -35,4 +40,75 @@
 	<!-- End of Tab panes from left menu -->	
 </div>
 <!-- END OF RIGHT CPNTENT -->
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		LoadTrips(0);
+	})
+
+	function LoadTrips(index){
+		var offset = index * 10;
+		$.ajax({
+			url: "../phps/show_cabRides.php",
+			type: "POST",
+			data: {offset: offset},
+			success: function(msg){
+				//alert(msg);
+				var a = JSON.parse(msg);
+				$('#listcabRides').empty();
+				$(".pagination ul").empty();
+				if(a.length > 0){
+					for(var i=1; i<a.length; i++){
+						var data = "<tr>";
+						data += "<td>"+a[i].drName+"</td>";
+						data += "<td>"+a[i].cabtype+"</td>";
+						data += "<td>"+a[i].cabno+"</td>";
+						data += "<td>"+a[i].pick_place+"</td>"; 
+						data += "<td>"+a[i].pick_date+"</td>"; 
+						data += "<td>"+a[i].drop_place+"</td>"; 
+						data += "<td>"+a[i].drop_date+"</td>";
+						data += "<td>"+a[i].distance+"</td>";
+						data += "<td>"+a[i].fee+"</td>";
+						data += "<td>"+a[i].commission+"</td>";
+						data += "</tr>";
+						$("#listcabRides").append(data);
+					}
+					var pages = a[0].pages;
+					if(pages > 1){
+						var pageData = '';
+						if(index != 0){
+							pageData += '<li class="double-sym" onclick="LoadTrips(0)">&#xab;</li>';
+							var previous = index - 1;
+							pageData += '<li onclick="LoadTrips('+previous+')">&#x276E;</li>';
+						}
+						for(var j=1; j<=pages; j++){
+							if((index + 1) == j){
+								pageData += '<li class="active" onclick="LoadTrips('+(j-1)+')">'+j+'</li>';
+							}else{
+								pageData += '<li onclick="LoadTrips('+(j-1)+')">'+j+'</li>';
+							}
+						}
+						if(index != (pages - 1)){
+							var next = index + 1;
+							pageData += '<li onclick="LoadTrips('+next+')">&#x276F;</li>';
+							var lastIndex = pages - 1;
+							pageData += '<li class="double-sym" onclick="LoadTrips('+lastIndex+')">&#xbb;</li>'
+						}
+						$(".pagination ul").append(pageData);
+					}
+				}else{
+					$("#listcabRides").append("<tr><td colspan='7' style='text-align:center;'>No Records found!</td></tr>");
+				}
+			},
+			error: function(error){
+				if(error.status == "0"){
+					alert("Unable to connect to server, Try again");
+				}else{
+					alert("Something went wrong, Try again");
+				}
+			}
+		});
+	}
+</script>
+
 <?php include 'footer.php'; ?>
--- web/Admin/phps/show_cabRides.php
+++ web/Admin/phps/show_cabRides.php
@@ -1,40 +1,56 @@
 <?php
 include '../../Includes/db.php';
-if(isset($_POST['filter'])){
-	$query8 = mysqli_query($conn, "select * from tbl_customer_trips");
+/*if(isset($_POST['filter'])){
+	$query8 = mysqli_query($conn, "select driver_id, booking_id, start_place, start_date, start_time, end_place, end_date, end_time, total_distance, total_fee, owner_commission from tbl_customer_trips order by start_date desc, start_time desc");
 }else{
-	$query8 = mysqli_query($conn, "select * from tbl_customer_trips");
-}
+	$query8 = mysqli_query($conn, "select driver_id, booking_id, start_place, start_date, start_time, end_place, end_date, end_time, total_distance, total_fee, owner_commission from tbl_customer_trips order by start_date desc, start_time desc");
+}*/
+
+$query1 = mysqli_query($conn, "select count(s_no) as count from tbl_customer_trips");
+$row1 = mysqli_fetch_assoc($query1);
+$pages = $row1['count'];
+$limit = 10;
+$pages = ceil($pages / $limit);
+
+$offset = $_POST['offset'];
+
+$query8 = mysqli_query($conn, "select driver_id, booking_id, start_place, start_date, start_time, end_place, end_date, end_time, total_distance, total_fee, owner_commission from tbl_customer_trips order by start_date desc, start_time desc limit ".$offset.", ".$limit);
 $num8 = mysqli_num_rows($query8);
 $outp = '[';
-if($num8 != 0){
-	$s_no = 1;
+if($num8 > 0){
+	$outp .= '{"pages": "'.$pages.'"}';
 	while($row8 = mysqli_fetch_assoc($query8)){
-		
-		$query9 = mysqli_query($conn, "select * from tbl_drivers where driver_id='".$row8['driver_id']."'");
+		$driverID = $row8['driver_id'];
+		$bookingID = $row8['booking_id'];
+		$startplace = $row8['start_place'];
+		$startdt = $row8['start_date'];
+		$starttm = $row8['start_time'];
+		$endplace = $row8['end_place'];
+		$enddt = $row8['end_date'];
+		$endtm = $row8['end_time'];
+		$totaldistance= $row8['total_distance'];
+		$totalfee = $row8['total_fee'];
+		$ownercommission = $row8['owner_commission'];
+
+		$query9 = mysqli_query($conn, "select name, vehicle_category, vehicle_no from tbl_drivers where driver_id='".$driverID."'");
 		$num9 = mysqli_num_rows($query9);
 		$row9 = mysqli_fetch_assoc($query9);
 		$name = $row9['name'];
 		$cabtype = $row9['vehicle_category'];
 		$cabno = $row9['vehicle_no'];
 		
-		$query10 = mysqli_query($conn, "select * from tbl_cab_categories where category_id='".$cabtype."'");
+		$query10 = mysqli_query($conn, "select name from tbl_cab_categories where category_id='".$cabtype."'");
 		$num10 = mysqli_num_rows($query10);
 		$row10 = mysqli_fetch_assoc($query10);
 		$cabname = $row10['name'];
-		$startplace = $row8['start_place'];
-		$startdt = $row8['start_date'];
-		$starttm = $row8['start_time'];
+
 		if($startdt != ""){
 			$startdt = $startdt;
-			//$startdt = date("d M y", strtotime($startdt));
 			$startdt = $startdt." ".$starttm;
 		}else{
 			$startdt = "";
 		}
-		$endplace = $row8['end_place'];
-		$enddt = $row8['end_date'];
-		$endtm = $row8['end_time'];
+		
 		if($enddt != ""){
 			$enddt = $enddt;
 			//$enddt = date("d M y", strtotime($enddt));
@@ -42,26 +58,20 @@ if($num8 != 0){
 		}else{
 			$enddt = "";
 		}
-		$totaldistance= $row8['total_distance'];
-		$totalfee = $row8['total_fee'];
-		$ownercommission = $row8['owner_commission'];
 
 		if($outp != '['){$outp .= ',';}
-		$outp .= '{"sno":"'.$s_no.'",';
-		$outp .= '"driver_name":"'.$name.'",';
+
+		$outp .= '{"bookingID":"'.$bookingID.'",';
+		$outp .= '"drName":"'.$name.'",';
 		$outp .= '"cabtype":"'.$cabtype.'",';
 		$outp .= '"cabno":"'.$cabno.'",';
 		$outp .= '"pick_place":"'.$startplace.'",';
 		$outp .= '"pick_date":"'.$startdt.'",';
-		$outp .= '"pick_time":"'.$starttm.'",';
 		$outp .= '"drop_place":"'.$endplace.'",';
 		$outp .= '"drop_date":"'.$enddt.'",';
-		$outp .= '"drop_time":"'.$endtm.'",';
-		$outp .= '"total_distance":"'.$totaldistance.'",';
-		$outp .= '"total_fee":"'.$totalfee.'",';
-		$outp .= '"owner_commission":"'.$ownercommission.'"}';
-		
-		$s_no= $s_no+1;
+		$outp .= '"distance":"'.$totaldistance.'",';
+		$outp .= '"fee":"'.$totalfee.'",';
+		$outp .= '"commission":"'.$ownercommission.'"}';
 	}
 }
 $outp .= ']';
